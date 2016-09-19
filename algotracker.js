51c4a82888cfa1f8a828d42c1ea72bae2d4b43be
ksavransky <ksavransky@gmail.com>
2016-09-19 13:11:30 -0700

    three sorts working

--- algotracker.js
+++ algotracker.js
@@ -6,6 +6,26 @@ for(let i = 0; i < 40000; i++){
 }
 
 
+function timeTracker(algo, arg){
+  let startTime = new Date();
+  let result = algo(arg);
+  let endTime = new Date();
+  console.log(endTime - startTime); //this is in ms
+  return result;
+}
+
+console.log("bubbleSort:");
+console.log(timeTracker(bubbleSort, test.slice()));
+
+console.log("mergeSort:");
+console.log(timeTracker(mergeSort, test.slice()));
+//
+console.log("quickSort:");
+console.log(timeTracker(quickSort, test.slice()));
+
+
+// THE SORTING ALGORITHMS
+
 
 function bubbleSort(array){
   let sorted = false;
@@ -24,12 +44,47 @@ function bubbleSort(array){
   return array;
 }
 
-function timeTracker(algo, arg){
-  let startTime = new Date();
-  let result = algo(arg);
-  let endTime = new Date();
-  console.log(endTime - startTime); //this is in ms
-  return result;
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  } else {
+    const middle = Math.floor(array.length / 2);
+
+    const left = mergeSort(array.slice(0, middle));
+    const right = mergeSort(array.slice(middle));
+
+    return merge(left, right);
+  }
 }
 
-console.log(timeTracker(bubbleSort, test));
+function merge(left, right) {
+  const merged = [];
+
+  while (left.length > 0 && right.length > 0) {
+    let nextItem = (left[0] < right[0]) ? left.shift() : right.shift();
+    merged.push(nextItem);
+  }
+
+  return merged.concat(left, right);
+}
+
+
+function quickSort(array){
+  if (array.length < 2) {
+    return array;
+  }
+
+    const pivot = array[0];
+    const left = [];
+    const right = [];
+
+    for(let i = 1; i < array.length; i++){
+      if(array[i] <= pivot){
+        left.push(array[i]);
+      } else {
+        right.push(array[i]);
+      }
+    }
+
+    return quickSort(left).concat([pivot]).concat(quickSort(right));
+}
