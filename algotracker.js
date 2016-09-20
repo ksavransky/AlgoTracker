8dba44c269123ae815a9b7a7cb3e9da0f8a47482
ksavransky <ksavransky@gmail.com>
2016-09-19 22:13:08 -0700

    trying to only get one graph

--- algotracker.js
+++ algotracker.js
@@ -1,3 +1,7 @@
+var axisScale = "linear";
+
+
+
 var dataset = [];
 
 let algos = [bubbleSort, mergeSort, quickSort];
@@ -31,7 +35,7 @@ function timeTracker(algo, arg){
   let timeElapsed = endTime - startTime;
   console.log(algo);
   console.log(timeElapsed); //this is in ms
-  dataset.push([arg.length, timeElapsed]);
+  dataset.push([arg.length, timeElapsed, algo.name]);
   console.log(result);
 }
 
@@ -103,81 +107,112 @@ function quickSort(array){
 
 
 // drawing
+
+
+
 function draw(){
 
-var w = 500;
-var h = 300;
-var padding = 30;
+  if(document.getElementById("axis-scale").value === "sqrt"){
+    axisScale = "sqrt";
+  } else {
+    axisScale = "linear";
+  }
 
-var svg = d3.select("div.sort-graph")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+  var w = 700;
+  var h = 700;
+  var padding = 50;
 
-var xScale = d3.scale.linear()
-               .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-               .range([padding, w - padding * 2]);
-
-var yScale = d3.scale.linear()
-                .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-                .range([h - padding, padding]);
-
-var rScale = d3.scale.linear()
-               .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-               .range([2, 5]);
+  var svg = d3.select("div.sort-graph")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h);
 
 
+  var xScale = d3.scale.linear()
+  if(axisScale === "sqrt"){
+    xScale = d3.scale.sqrt()
+  }
+  xScale.domain([0, d3.max(dataset, function(d) { return d[0]; })])
+         .range([padding, w - padding * 2]);
 
-svg.selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append("circle")
-   .attr("cx", function(d) {
-     return xScale(d[0]);
-   })
-   .attr("cy", function(d) {
-     return yScale(d[1]);
-   })
-   .attr("r", function(d) {
-     return rScale(d[1]);
-   });
+  var yScale = d3.scale.linear()
+  if(axisScale === "sqrt"){
+    yScale = d3.scale.sqrt()
+  }
+  yScale.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+        .range([h - padding, padding]);
 
-
-svg.selectAll("text")
-   .data(dataset)
-   .enter()
-   .append("text")
-   .text(function(d) {
-      return d[0] + "," + d[1];
-    })
-    .attr("x", function(d) {
-     return xScale(d[0]);
-    })
-    .attr("y", function(d) {
-         return yScale(d[1]);
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "11px")
-    .attr("fill", "red");
+  var rScale = d3.scale.linear()
+  if(axisScale === "sqrt"){
+    rScale = d3.scale.sqrt()
+  }
+  rScale.domain([0, d3.max(dataset, function(d) { return d[1]; })])
+        .range([2, 5]);
 
 
-svg.append("g")
-       .attr("class", "axis")
-       .attr("transform", "translate(0," + (h - padding) + ")")
-       .call(d3.svg.axis()
-                   .scale(xScale)
-                   .orient("bottom")
-                   .ticks(5));
 
-var yAxis = d3.svg.axis()
-                 .scale(yScale)
-                 .orient("left")
-                 .ticks(5);
+  svg.selectAll("circle")
+     .data(dataset)
+     .enter()
+     .append("circle")
+     .attr("cx", function(d) {
+       return xScale(d[0]);
+     })
+     .attr("cy", function(d) {
+       return yScale(d[1]);
+     })
+     .attr("r", 5)
+     .attr("fill", function(d) {
+       if(d[2] === "bubbleSort"){
+         return "red";
+       } else if(d[2] === "mergeSort"){
+         return "blue";
+       } else {
+         return "yellow";
+       }
+     });
 
-svg.append("g")
-   .attr("class", "axis")
-   .attr("transform", "translate(" + padding + ",0)")
-   .call(yAxis);
+
+  svg.selectAll("text")
+     .data(dataset)
+     .enter()
+     .append("text")
+     .text(function(d) {
+        return d[0] + "," + d[1];
+      })
+      .attr("x", function(d) {
+       return xScale(d[0]);
+      })
+      .attr("y", function(d) {
+           return yScale(d[1]);
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("fill", "red");
+
+
+  svg.append("g")
+         .attr("class", "axis")
+         .attr("transform", "translate(0," + (h - padding) + ")")
+         .call(d3.svg.axis()
+                     .scale(xScale)
+                     .orient("bottom")
+                     .ticks(6));
+
+  var yAxis = d3.svg.axis()
+                   .scale(yScale)
+                   .orient("left")
+                   .ticks(6);
+
+  svg.append("g")
+     .attr("class", "axis")
+     .attr("transform", "translate(" + padding + ",0)")
+     .call(yAxis);
+     // 
+    //  let graph = document.getElementsByClassName('sort-graph');
+    //  if(graph.childElementCount() > 1){
+    //    graph.removeChild(graph.firstChild);
+    //  }
 }
 
 runSort();
