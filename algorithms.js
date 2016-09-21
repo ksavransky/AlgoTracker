babe8c3a75755af666bcb45d17bf29879f05deee
ksavransky <ksavransky@gmail.com>
2016-09-21 09:30:59 -0700

    styling and more algos

--- algorithms.js
+++ algorithms.js
@@ -61,3 +61,91 @@ function quickSort(array){
 
     return quickSort(left).concat([pivot]).concat(quickSort(right));
 }
+
+
+function insertionSort(array) {
+    var len = array.length;
+    for (var i = 0; i < len; i++) {
+        var tmp = array[i]; //Copy of the current element.
+        /*Check through the sorted part and compare with the number in tmp. If large, shift the number*/
+        for (var j = i - 1; j >= 0 && (array[j] > tmp); j--) {
+            //Shift the number
+            array[j + 1] = array[j];
+        }
+        //Insert the copied number at the correct position
+        //in sorted part.
+        array[j + 1] = tmp;
+    }
+    return array;
+}
+
+
+function selectionSort(array) {
+    var length = array.length;
+    for (var i = 0; i < length - 1; i++) {
+        //Number of passes
+        var min = i; //min holds the current minimum number position for each pass; i holds the Initial min number
+        for (var j = i + 1; j < length; j++) { //Note that j = i + 1 as we only need to go through unsorted array
+            if (array[j] < array[min]) { //Compare the numbers
+                min = j; //Change the current min number position if a smaller num is found
+            }
+        }
+        if (min != i) {
+            //After each pass, if the current min num != initial min num, exchange the position.
+            //Swap the numbers
+            var tmp = array[i];
+            array[i] = array[min];
+            array[min] = tmp;
+        }
+    }
+    return array;
+}
+
+
+// HEAP SORT
+var arrayLength;
+
+function buildHeap(input) {
+    arrayLength = input.length;
+
+    for (var i = Math.floor(arrayLength / 2); i >= 0; i -= 1) {
+        heapify(input, i);
+    }
+}
+
+function heapify(input, i) {
+    var left = 2 * i + 1;
+    var right = 2 * i + 2;
+    var largest = i;
+
+    if (left < arrayLength && input[left] > input[largest]) {
+        largest = left;
+    }
+
+    if (right < arrayLength && input[right] > input[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(input, i, largest);
+        heapify(input, largest);
+    }
+}
+
+function swap(input, index_A, index_B) {
+    var temp = input[index_A];
+
+    input[index_A] = input[index_B];
+    input[index_B] = temp;
+}
+
+function heapSort(input) {
+    buildHeap(input);
+
+    for (var i = input.length - 1; i > 0; i--) {
+        swap(input, 0, i);
+        arrayLength--;
+        heapify(input, 0);
+    }
+    return input;
+}
