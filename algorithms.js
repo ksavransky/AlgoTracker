2cd6489591b8d74f1340fd77fa49c769d041a229
ksavransky <ksavransky@gmail.com>
2016-09-29 11:15:45 -0700

    bigger textboxs

--- algorithms.js
+++ algorithms.js
@@ -105,50 +105,50 @@ function selectionSort(array) {
 // HEAP SORT
 var arrayLength;
 
-function buildHeap(input) {
-    arrayLength = input.length;
-
-    for (var i = Math.floor(arrayLength / 2); i >= 0; i -= 1) {
-        heapify(input, i);
-    }
-}
-
-function heapify(input, i) {
-    var left = 2 * i + 1;
-    var right = 2 * i + 2;
-    var largest = i;
-
-    if (left < arrayLength && input[left] > input[largest]) {
-        largest = left;
-    }
-
-    if (right < arrayLength && input[right] > input[largest]) {
-        largest = right;
-    }
-
-    if (largest != i) {
-        swap(input, i, largest);
-        heapify(input, largest);
-    }
-}
-
-function swap(input, index_A, index_B) {
-    var temp = input[index_A];
-
-    input[index_A] = input[index_B];
-    input[index_B] = temp;
-}
-
-function heapSort(input) {
-    buildHeap(input);
-
-    for (var i = input.length - 1; i > 0; i--) {
-        swap(input, 0, i);
-        arrayLength--;
-        heapify(input, 0);
-    }
-    return input;
-}
+// function buildHeap(input) {
+//     arrayLength = input.length;
+//
+//     for (var i = Math.floor(arrayLength / 2); i >= 0; i -= 1) {
+//         heapify(input, i);
+//     }
+// }
+//
+// function heapify(input, i) {
+//     var left = 2 * i + 1;
+//     var right = 2 * i + 2;
+//     var largest = i;
+//
+//     if (left < arrayLength && input[left] > input[largest]) {
+//         largest = left;
+//     }
+//
+//     if (right < arrayLength && input[right] > input[largest]) {
+//         largest = right;
+//     }
+//
+//     if (largest != i) {
+//         swap(input, i, largest);
+//         heapify(input, largest);
+//     }
+// }
+//
+// function swap(input, index_A, index_B) {
+//     var temp = input[index_A];
+//
+//     input[index_A] = input[index_B];
+//     input[index_B] = temp;
+// }
+//
+// function heapSort(input) {
+//     buildHeap(input);
+//
+//     for (var i = input.length - 1; i > 0; i--) {
+//         swap(input, 0, i);
+//         arrayLength--;
+//         heapify(input, 0);
+//     }
+//     return input;
+// }
 
 
 // MORE Algos
@@ -215,3 +215,55 @@ function subsets(array) {
   const subs = subsets(array.slice(0, array.length - 1));
   return subs.concat(subs.map(sb => sb.concat([lastElement])));
 }
+
+function heapSort(arr){
+        let last = arr.length - 1;
+        function parent(idx){
+          if(idx > 0 && idx <= last){return ~~((idx -1) / 2);}
+        }
+        function children(idx){
+          let children = {l: undefined, r: undefined}
+          if(2 * idx + 1 <= last){children.l = 2 * idx + 1;}
+          if(2 * idx + 2 <= last){children.r = 2 * idx + 2;}
+          return children
+        }
+        function swap(idx1, idx2){
+          let temp = arr[idx1];
+          arr[idx1] = arr[idx2];
+          arr[idx2] = temp;
+        }
+        function pair(idx){
+          if(idx < 1){return undefined}
+          return children(parent(idx));
+        }
+
+        function heapStep(idx){
+          let l = pair(idx).l;
+          let r = pair(idx).r;
+          let par = parent(idx);
+          let max = arr[l];
+          let maxi = l;
+          if(r && l && arr[r] > arr[l]){
+            max = arr[r];
+            maxi = r;
+          }
+          if(max > arr[par]){swap(maxi, par);}
+          if(children(maxi).l){heapStep(children(maxi).l);}
+        }
+
+        function makeHeap(n){
+          for (let i = n; i > 0; i-=2) {
+            last = n;
+            heapStep(i);
+          }
+        }
+
+        makeHeap(last)
+
+        for (let i = last; i > 0; i--) {
+          swap(0, i);
+          last--;
+          heapStep(1);
+        }
+return arr;
+      }
